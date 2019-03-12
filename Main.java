a42f15538b62e269a0bf60950cdbbd66eb89d960
LentoKun <33175224+Hytlenz@users.noreply.github.com>
2019-03-12 18:41:10 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-
 package bms;
 
 import javax.swing.*;
@@ -6,8 +5,6 @@ import java.awt.*;
 import java.awt.event.*;
 import java.sql.*;
 import javax.swing.table.DefaultTableModel;
-import java.awt.image.*;
-
 
 /**
  * _________________________________________________________________________
@@ -19,7 +16,7 @@ import java.awt.image.*;
  * - v1.1 - Connecting SQL
  * - v1.2 - Cleanup and Added Tables
  * - v1.3 - Adding Functions of Buttons
- * _________________________________________________________________________
+ * ________________________________________________________________________
  */
 public class Main {
 
@@ -27,55 +24,47 @@ public class Main {
         Class.forName("com.mysql.jdbc.Driver");
         
         //Main Core of JFrame
-        
         JFrame frame_main = new JFrame("Book Monitoring System - Main Form");
          frame_main.setLayout(new FlowLayout());
-         frame_main.setSize(800,500);
+         frame_main.setSize(500,500);
          frame_main.setLocationRelativeTo(null);
          frame_main.setVisible(true);
-         
-         ImageIcon img = new ImageIcon("images/bms_icon.png");
-         frame_main.setIconImage(img.getImage());
-        
-         
-        JLabel labelMain = new JLabel(
-                "<html>Welcome to Book Monitoring System<br> Coded by: Group IV of 11 - PASCAL<br> Version: v1.3</html>"
-        );
-        Font fo = new Font("Times New Roman",Font.BOLD,20);
+          
+        JLabel labelMain = new JLabel("WELCOME TO");
+        Font fo = new Font("Comic Sans MS",Font.BOLD,20);
          labelMain.setFont(fo);
+         labelMain.setForeground(Color.GREEN);
+         labelMain.setBounds(430,20,300,100);
+         
+        JLabel label1 = new JLabel("BOOK");
+        Font fon = new Font("Comic Sans MS",Font.BOLD,20);
+         label1.setFont(fon);
+         label1.setForeground(Color.BLUE);
+         label1.setBounds(470,95,150,50);
+         
+        JLabel label2 = new JLabel("MONITORING SYSTEM");
+        Font font = new Font("Comic Sans MS",Font.BOLD,20);
+         label2.setFont(font);
+         label2.setForeground(Color.BLUE);
+         label2.setBounds(380,150,600,50);
         
         JPanel panel_main = new JPanel();
-         panel_main.setBackground(Color.BLACK);
-         panel_main.setPreferredSize(new Dimension(800,500));
+         panel_main.setLayout(null);
+         panel_main.setBounds(10,50,60,20);
+         panel_main.setBackground(Color.GRAY);
+         panel_main.setPreferredSize(new Dimension(1000,500));
+         panel_main.add(labelMain);
+         panel_main.add(label1);
+         panel_main.add(label2);
         
-        JPanel panel_choose = new JPanel(new GridBagLayout());
-         panel_choose.setBackground(Color.WHITE);
-         panel_choose.setPreferredSize(new Dimension(400,300));
-         panel_choose.add(labelMain);
-        
-        //JTextField txt1 = new JTextField();
-        //txt1.setPreferredSize(new Dimension(100,30));
-        
-        GridBagConstraints grid = new GridBagConstraints();
-        
-        
- /*
- _________________________________________________________________________
-   ___       __  __              ___
-  / _ )__ __/ /_/ /____  ___    <  /
- / _  / // / __/ __/ _ \/ _ \   / / 
-/____/\_,_/\__/\__/\___/_//_/  /_/  
-                                    
-_________________________________________________________________________
-  */
-        JButton buttonMain1 = new JButton("LIST OF BORROWED BOOKS");
-        
-
-         buttonMain1.setBounds(150, 220, 100, 25);
-         buttonMain1.addActionListener(new ActionListener(){
+        // button for borrowed books
+        JButton buttonBorrowed = new JButton("LIST OF BORROWED BOOKS");
+         buttonBorrowed.setBounds(350, 250, 150, 50);
+         buttonBorrowed.setBackground(Color.WHITE);
+         buttonBorrowed.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
-                frame_main.setVisible(true);
+                frame_main.setVisible(false);
                 
         try{
             Connection conn = DriverManager.getConnection(
@@ -86,13 +75,20 @@ _________________________________________________________________________
                 
                 JFrame frame_listbor = new JFrame("Book Monitoring System - List of Borrowed Books");
                  frame_listbor.setLayout(new FlowLayout());
-                 frame_listbor.setSize(800,500);
+                 frame_listbor.setSize(1000,500);
                  frame_listbor.setLocationRelativeTo(null);
                  frame_listbor.setVisible(true);
                  
+                JPanel panel_listbor = new JPanel();
+                 panel_listbor.setPreferredSize(new Dimension(1000,500));
+                 panel_listbor.setBackground(Color.BLACK);
+                 
                  DefaultTableModel model = new DefaultTableModel();
                  JTable table = new JTable(model);
-           
+                  table.setGridColor(Color.BLACK);
+                  table.setBounds(600,200,300,300);
+                  table.setShowGrid(true);
+                
                   model.addColumn("ID");
                   model.addColumn("Book Name");
                   model.addColumn("Book Author");
@@ -114,15 +110,31 @@ _________________________________________________________________________
                  JTextField textBookauthor = new JTextField();
                  JTextField textUser = new JTextField();
                  
+                 JLabel labelId = new JLabel("[ID]");
+                 JLabel labelBook = new JLabel("[BOOK NAME]");
+                 JLabel labelAuthor = new JLabel("[BOOK AUTHOR]");
+                 JLabel labelUser = new JLabel("[BORROWED USER]");
+                 
+                 labelId.setBounds(100, 200, 1500, 25);
+                 labelBook.setBounds(20, 250, 100, 25);
+                 labelAuthor.setBounds(20, 280, 100, 25);
+                 labelUser.setBounds(20, 310, 100, 25);
+                 
+                 panel_listbor.add(labelId);
+                 //frame_listbor.add(labelId);
+                 //frame_listbor.add(labelBook);
+                 //frame_listbor.add(labelAuthor);
+                 //frame_listbor.add(labelUser);
+
                  textId.setPreferredSize(new Dimension(100,30));
                  textBookname.setPreferredSize(new Dimension(100,30));
                  textBookauthor.setPreferredSize(new Dimension(100,30));
                  textUser.setPreferredSize(new Dimension(100,30));
                  
-                 textId.setBounds(20, 220, 100, 25);
-                 textBookname.setBounds(20, 250, 100, 25);
-                 textBookauthor.setBounds(20, 280, 100, 25);
-                 textUser.setBounds(20, 310, 100, 25);
+                 textId.setBounds(800, 250, 150, 500);
+                 textBookname.setBounds(700, 250, 100, 500);
+                 textBookauthor.setBounds(700, 280, 100, 30);
+                 textUser.setBounds(700, 310, 100, 25);
            
                 JScrollPane scroll = new JScrollPane(table);
                  frame_listbor.add(scroll,BorderLayout.CENTER);
@@ -169,40 +181,48 @@ _________________________________________________________________________
                      }  
                     }
                 });
+                 
+                JButton buttonBack = new JButton("BACK TO MENU");
+                 buttonBack.setToolTipText("BACK TO THE MAIN MENU OF THE SYSTEM");
+                 buttonBack.addActionListener(new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent e){
+                        
+                        frame_main.setVisible(true);
+                        frame_listbor.setVisible(false);
+                        
+                    }
+                });
                 
                 // - Output for buttonMain1 - 
                 //panel.add(label);
-                frame_listbor.add(buttonAdd);
-                frame_listbor.add(buttonDel);
-                frame_listbor.add(textId);
-                frame_listbor.add(textBookname);
-                frame_listbor.add(textBookauthor);
-                frame_listbor.add(textUser);
+                //frame_listbor.add(buttonAdd);
+                //frame_listbor.add(buttonDel);
+                //frame_listbor.add(buttonBack);
+                frame_listbor.add(panel_listbor);
+                //panel_listbor.add(textId);
+                panel_listbor.add(table);
+                panel_listbor.add(textId);
+                //frame_listbor.add(textBookname);
+                //frame_listbor.add(textBookauthor);
+                //frame_listbor.add(textUser);
+                frame_listbor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 
                 } catch(Exception ex){
+                
             
              }
             }
         });
-        // ==================== END OF LINE IN BUTTON 1 ===================== //
-        // ==================== END OF LINE IN BUTTON 1 ===================== //
-        
-        
- /*
-_________________________________________________________________________
-   ___       __  __              ___ 
-  / _ )__ __/ /_/ /____  ___    |_  |
- / _  / // / __/ __/ _ \/ _ \  / __/ 
-/____/\_,_/\__/\__/\___/_//_/ /____/ 
-                                     
-_________________________________________________________________________
-  */
-        JButton buttonMain2 = new JButton("LIST OF BOOKS");
-         buttonMain2.setBounds(150, 310, 100, 25);
-         buttonMain2.addActionListener(new ActionListener(){
+          
+        // button for books 
+        JButton buttonBooks = new JButton("LIST OF BOOKS");
+         buttonBooks.setBounds(550, 250, 150,50);
+         buttonBooks.setBackground(Color.WHITE);
+         buttonBooks.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
-                frame_main.setVisible(true);
+                frame_main.setVisible(false);
                 
         try{
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_books?zeroDateTimeBehavior=convertToNull","root","");
@@ -258,8 +278,8 @@ _________________________________________________________________________
                 
                 // - Button Add For Books -
                 JButton buttonAdd = new JButton("REGISTER BOOK");
-                buttonAdd.setToolTipText("REGISTERS A BOOK TO ADD IN TABLE");
-                buttonAdd.addActionListener(new ActionListener(){
+                 buttonAdd.setToolTipText("REGISTERS A BOOK TO ADD IN TABLE");
+                 buttonAdd.addActionListener(new ActionListener(){
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         
@@ -275,8 +295,8 @@ _________________________________________________________________________
                 
                 // - Button Delete For Books - 
                 JButton buttonDel = new JButton("DELETE BOOK");
-                buttonDel.setToolTipText("DELETES A BOOK IN THE TABLE");
-                buttonDel.addActionListener(new ActionListener(){
+                 buttonDel.setToolTipText("DELETES A BOOK IN THE TABLE");
+                 buttonDel.addActionListener(new ActionListener(){
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         
@@ -287,42 +307,119 @@ _________________________________________________________________________
                         } else {
                        System.out.println("Delete Error");
                         }
+                        
                     }
                 });
                 
-                /**JLabel label = new JLabel(
-                 "<html>W.I.P<BR>JUST AN IDEA<BR>TABLE OF BOOKS AND REGISTER SOME BOOK</html>"
-                 );
-                frame_listbooks.add(label);
-                */
+                JButton buttonBack = new JButton("BACK TO MENU");
+                 buttonBack.setToolTipText("BACK TO THE MAIN MENU OF THE SYSTEM");
+                 buttonBack.addActionListener(new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent e){
+                        
+                        frame_main.setVisible(true);
+                        frame_listbooks.setVisible(false);
+                        
+                    }
+                });
+            
                
-                
                 frame_listbooks.add(buttonAdd);
                 frame_listbooks.add(buttonDel);
+                frame_listbooks.add(buttonBack);
                 frame_listbooks.add(textId);
                 frame_listbooks.add(textBookname);
                 frame_listbooks.add(textBookauthor);
                 frame_listbooks.add(textCategory);
+                frame_listbooks.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                  
                 } catch(Exception ex){
             
              }
             }
         });
-        // ==================== END OF LINE IN BUTTON 2 ===================== //
-        // ==================== END OF LINE IN BUTTON 2 ===================== //
+         
+         // button for auth
+         JButton buttonAuth = new JButton("Lets Go!");
+          buttonAuth.setBounds(420, 250, 150, 50);
+          buttonAuth.setBackground(Color.WHITE);
+          buttonAuth.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame_main.setVisible(false);
+                
+                JFrame frame_auth = new JFrame("Book Monitoring System - Authentication Form");
+                 frame_auth.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                 frame_auth.setLayout(new FlowLayout());
+                 frame_auth.setSize(500,500);
+                 frame_auth.setLocationRelativeTo(null);
+                 frame_auth.setVisible(true);
+                 
+                JLabel label1 = new JLabel("> User:");
+                 Font fo = new Font("Calibri (Body)",Font.BOLD,12);
+                 label1.setForeground(Color.WHITE);
+                 label1.setFont(fo);
+                 label1.setBounds(50,20,300,100);
+         
+                JLabel label2 = new JLabel("> Password:");
+                 Font fon = new Font("Calibri (Body)",Font.BOLD,12);
+                 label2.setForeground(Color.WHITE);
+                 label2.setFont(fon);
+                 label2.setBounds(50,95,150,50);
+                 
+                JPanel panel_log = new JPanel();
+                 panel_log.setLayout(null);
+                 panel_log.setBounds(10,50,60,20);
+                 panel_log.setBackground(Color.GRAY);
+                 panel_log.setPreferredSize(new Dimension(500,500));
+                 
+                 JLabel label3 = new JLabel("LOG - IN");
+                  Font font = new Font("Comic Sans MS",Font.BOLD,20);
+                  label3.setFont(font);
+                  label3.setForeground(Color.WHITE);
+                  label3.setBounds(10,20,150,20);
+                  label3.add(panel_log);
+        
+                JPanel panel_auth = new JPanel();
+                 panel_auth.setLayout(null);
+                 panel_auth.setBounds(10,50,60,20);
+                 panel_auth.setBackground(Color.GRAY);
+                 panel_auth.setPreferredSize(new Dimension(500,500));
+                 panel_auth.add(label1);
+                 panel_auth.add(label2);
+                 
+                JTextField user = new JTextField();
+                 user.setBounds(150,50,150,30);
+                JTextField pass = new JTextField();
+                 pass.setBounds(150,100,150,30);
+                 
+                 JButton buttonLogin = new JButton("Login Access");
+                  buttonLogin.setBounds(50,160,150,30);
+                 JButton buttonRegis = new JButton("Register");
+                  buttonRegis.setBounds(50,200,150,30); 
+                 
+                 
+                 frame_auth.add(panel_auth);
+                 panel_auth.add(user);
+                 panel_auth.add(pass);
+                 panel_auth.add(buttonLogin);
+                 panel_auth.add(buttonRegis);
+                 panel_auth.add(panel_log);
+               
+                
+            }
+         });
         
         // - Main Output for Whole Program -
         frame_main.add(panel_main);
-        panel_main.add(panel_choose);
         //pn1.add(txt1);
-        grid.insets = new Insets(10,10,10,10);
-         grid.gridx = 0; grid.gridy = 1;
-        panel_choose.add(buttonMain1, grid);
-         grid.gridx = 0; grid.gridy = 2; 
-        panel_choose.add(buttonMain2, grid);
-         
-        
+        frame_main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //panel_main.add(buttonAuth);
+        panel_main.add(buttonBooks);
+        panel_main.add(buttonBorrowed);
+           
     }
     
 }
+
+
